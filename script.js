6be2206099b39c9c29caa0fb978c50d1b03fc2d7
Nick Bos <roofingbos@gmail.com>
2022-06-10 12:58:59 +0930

    feat: :sparkles: Add function that populates calculator display when number keys are clicked

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function divide(a, b) {
 function operate(operator, a, b) {
   return operator(a, b);
 }
+
+//number key functions
+function numInput(e) {
+  let display = document.getElementById("calc-display");
+  let obj = e.target;
+  display.textContent += obj.textContent;
+}
+
+let display = document.getElementById("calc-display");
+let num1 = document.getElementById("1-btn");
+let num2 = document.getElementById("2-btn");
+let num3 = document.getElementById("3-btn");
+let num4 = document.getElementById("4-btn");
+let num5 = document.getElementById("5-btn");
+let num6 = document.getElementById("6-btn");
+let num7 = document.getElementById("7-btn");
+let num8 = document.getElementById("8-btn");
+let num9 = document.getElementById("9-btn");
+
+num1.addEventListener("click", numInput);
+num2.addEventListener("click", numInput);
+num3.addEventListener("click", numInput);
+num4.addEventListener("click", numInput);
+num5.addEventListener("click", numInput);
+num6.addEventListener("click", numInput);
+num7.addEventListener("click", numInput);
+num8.addEventListener("click", numInput);
+num9.addEventListener("click", numInput);
